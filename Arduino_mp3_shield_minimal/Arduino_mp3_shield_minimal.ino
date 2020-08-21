c89a93698002bb6acb7feb8df3107586aefe4d6a
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2020-08-21 11:35:27 +0200

    Update Arduino_mp3_shield_minimal.ino

--- Arduino_mp3_shield_minimal/Arduino_mp3_shield_minimal.ino
+++ Arduino_mp3_shield_minimal/Arduino_mp3_shield_minimal.ino
@@ -1,72 +1,23 @@
-/**
- * \file demo.ino
- *
- * \brief Example sketch of using the VS1053 Arduino driver, demonstrating all methods and functions.
- * \remarks comments are implemented with Doxygen Markdown format
- *
- * \author Bill Porter
- * \author Michael P. Flaga
- *
- * This sketch listens for commands from a serial terminal (like the Serial
- * Monitor in the Arduino IDE). If it sees 1-9 it will try to play an MP3 file
- * named track00x.mp3 where x is a number from 1 to 9. For eaxmple, pressing
- * 2 will play 'track002.mp3'. A lowe case 's' will stop playing the mp3.
- * 'f' will play an MP3 by calling it by it's filename as opposed to a track
- * number.
- *
- * Sketch assumes you have MP3 files with filenames like "track001.mp3",
- * "track002.mp3", etc on an SD card loaded into the shield.
- */
-
 #include <SPI.h>
 #include <FreeStack.h>
-
-//Add the SdFat Libraries
 #include <SdFat.h>
-
-//and the MP3 Shield Library
 #include <vs1053_SdFat.h>
 
+/*
 // Below is not needed if interrupt driven. Safe to remove if not using.
 #if defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_Timer1
   #include <TimerOne.h>
 #elif defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_SimpleTimer
   #include <SimpleTimer.h>
 #endif
+*/
 
-/**
- * \brief Object instancing the SdFat library.
- *
- * principal object for handling all SdCard functions.
- */
 SdFat sd;
-
-/**
- * \brief Object instancing the vs1053 library.
- *
- * principal object for handling all the attributes, members and functions for the library.
- */
 vs1053 MP3player;
 
-//------------------------------------------------------------------------------
-/**
- * \brief Setup the Arduino Chip's feature for our use.
- *
- * After Arduino's kernel has booted initialize basic features for this
- * application, such as Serial port and MP3player objects with .begin.
- * Along with displaying the Help Menu.
- *
- * \note returned Error codes are typically passed up from MP3player.
- * Whicn in turns creates and initializes the SdCard objects.
- *
- * \see
- * \ref Error_Codes
- */
 void setup() {
 
-  
   Serial.begin(115200);
-
 
   //Initialize the SdCard.
   if(!sd.begin(SD_SEL, SPI_FULL_SPEED)) sd.initErrorHalt();
@@ -74,42 +25,10 @@ void setup() {
 
   //Initialize the MP3 Player Shield
   MP3player.begin();
-  
 
-  //MP3player.playTrack(1);
-
- 
-}
-
-//------------------------------------------------------------------------------
-/**
- * \brief Main Loop the Arduino Chip
- *
- * This is called at the end of Arduino kernel's main loop before recycling.
- * And is where the user's serial input of bytes are read and analyzed by
- * parsed_menu.
- *
- * Additionally, if the means of refilling is not interrupt based then the
- * MP3player object is serviced with the availaible function.
- *
- * \note Actual examples of the libraries public functions are implemented in
- * the parse_menu() function.
- */
-void loop() {
-
-// Below is only needed if not interrupt driven. Safe to remove if not using.
-/*#if defined(USE_MP3_REFILL_MEANS) \
-    && ( (USE_MP3_REFILL_MEANS == USE_MP3_SimpleTimer) \
-    ||   (USE_MP3_REFILL_MEANS == USE_MP3_Polled)      )
-
-  MP3player.available();
-#endif
-*/
+  // Play file track001.mp3
   MP3player.playTrack(1);
-  delay(100);
 }
 
 
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
+void loop() {}
