cea1f030a3b07725ca7a771312c8c22833997cbd
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2023-02-08 12:47:07 +0100

    Create FastLED_bare_bone_example.ino

---
+++ FastLED_bare_bone_example/FastLED_bare_bone_example.ino
@@ -0,0 +1,37 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 1
+#define DATA_PIN A5
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() 
+{
+  //initialize serial communications at a 9600 baud rate
+  Serial.begin(9600);
+
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  
+  // turn off the LED
+  leds[0] = CRGB(255,0,0);
+  FastLED.show();
+}
+
+void loop()
+{
+  // turn on the LED (red color)
+  leds[0] = CRGB(255,0,0);
+  FastLED.show();
+  
+  delay(1000); // wait 1 sec
+  
+  // turn off the LED
+  leds[0] = CRGB(0,0,0);
+  FastLED.show();
+  
+  delay(1000); // wait 1 sec
+  
+
+
+
+}
